a4dfd701f82707f8ea5add2ea45b86f65557e67f
rafaelrok <rafaelvieirasantos2006@hotmail.com>
2022-08-15 15:48:41 -0300

    Create api pagarme

---
+++ pagarme-api/src/main/java/br/com/rafaelvieira/pagarmeapi/PagarmeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelvieira.pagarmeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagarmeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagarmeApiApplication.class, args);
+    }
+
+}
---
+++ pagarme-api/src/test/java/br/com/rafaelvieira/pagarmeapi/PagarmeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelvieira.pagarmeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagarmeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
