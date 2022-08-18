bbb34ab54cc6558be7bff2118a8246bbdc85fc9a
rafaelrok <rafaelvieirasantos2006@hotmail.com>
2022-08-18 07:37:55 -0300

    Repair project payme-api

--- payme-api/src/main/java/br/com/rafaelvieira/paymeapi/PaymeApiApplication.java
+++ payme-api/src/main/java/br/com/rafaelvieira/paymeapi/PaymeApiApplication.java
@@ -3,9 +3,6 @@ package br.com.rafaelvieira.paymeapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author rafae
- */
 @SpringBootApplication
 public class PaymeApiApplication {
 
